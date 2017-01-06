301c332acfc3b64274e567bac9e039cf6aa91444
Anwesh43 <anweshthecool0@gmail.com>
2017-01-06 13:37:19 +0530

    Adding demo for listner

--- app/src/main/java/com/anwesome/ui/timerlibrarydemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/timerlibrarydemo/MainActivity.java
@@ -2,8 +2,11 @@ package com.anwesome.ui.timerlibrarydemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.anwesome.ui.circulartimermodule.CircularTimer;
+import com.anwesome.ui.circulartimermodule.TimeListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         CircularTimer circularTimer = new CircularTimer(this);
+        circularTimer.setTimerListener(new TimeListener() {
+            @Override
+            public void onStart() {
+                Toast.makeText(MainActivity.this,"Timer has started",Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onStop() {
+                Toast.makeText(MainActivity.this,"Timer has stopped",Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onTick(int time) {
+                Log.d("Time elapsed",""+time);
+            }
+        });
         circularTimer.start();
+
     }
 }
